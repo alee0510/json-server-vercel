a4be9b14ab744ddcccbd0c27a44b2ce5471b2df0
Ali Muksin <ali.muksin0510@gmail.com>
2025-07-28 12:20:01 +0700

    update config

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+module.exports = (req, res) => {
+  server(req, res);
+};
