a5c2fe0521c37253255c4c7f67b615133f727b4d
Ali Muksin <ali.muksin0510@gmail.com>
2025-07-28 12:57:41 +0700

    update cwd

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults();
 // allow write function
 const fs = require("fs");
 const path = require("path");
-const dbFile = path.join(__dirname, "db.json");
+const dbFile = path.resolve(process.cwd(), "db.json");
 
 const data = JSON.parse(fs.readFileSync(dbFile, "utf-8"));
 const router = jsonServer.router(data);
